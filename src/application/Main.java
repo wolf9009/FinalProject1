5badd17c51ee857732b9210b470cf5783b028136
Kongpobpisit Thermprateep <porsche.kt@hotmail.com>
2018-05-08 03:12:23 +0700

    ลองๆ

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,10 +11,11 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("KnightRun");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
