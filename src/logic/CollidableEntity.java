a3530cb31330763ce50eb88c205032b90decf11d
Sahapol <donglovemom@gmail.com>
2018-05-09 09:32:08 +0700

    update enermy and collidable entity

--- src/logic/CollidableEntity.java
+++ src/logic/CollidableEntity.java
@@ -1,5 +1,8 @@
 package logic;
 
+import javafx.scene.shape.Shape;
+
+//ยังไม่มี bullet
 public class CollidableEntity extends Entity {
 	protected double width ,height , collideDamage;
 	public int side;
@@ -10,7 +13,20 @@ public class CollidableEntity extends Entity {
 	
 	protected boolean collideWith(CollidableEntity other) {
 		if(this instanceof Bullet && other instanceof Bullet) {
-			return false;
+			return false;//ไม่ชนกระสุน
 		}
+		if ((this instanceof Bullet && other instanceof Items) || (this instanceof Items && other instanceof Bullet)) {
+			return false;//ไม่ชนกล่องไอเทม
+		}
+		if (this.side != other.side) {
+			Shape intersect = Shape.intersect(this.getBoundary(), other.getBoundary());
+			return (intersect.getBoundsInLocal().getWidth() != -1);
+		}
+		return false;//อันนี้ไม่เข้าใจ
+	}
+	
+	public abstract void onCollision(CollidableEntity others);//อันนี้ไม่เข้าใจ
+
+	public abstract Shape getBoundary();//อันนี้ไม่เข้าใจ
 	}
 }
--- src/logic/Enemy.java
+++ src/logic/Enemy.java
@@ -1,7 +1,16 @@
 package logic;
 
-public class Enemy extends CollidableEntity{
+public abstract class Enemy extends CollidableEntity{
+	private static int zCounter = -200;
 	public Enemy(double hp,double speed) {
+		super(hp,speed);
+		this.side = -1;
+		this.z = zCounter;
+		zCounter++;
+		if (zCounter > -100 ) {
+			zCounter = -200;
+		}
 		
+		GameLogic.currentEnermyNum++;
 	}
 }
